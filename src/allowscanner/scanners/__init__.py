c7eff3b15101ea48833c0dc967273dde651f4a57
eyren <airdropfindcm@gmail.com>
2026-06-14 18:21:07 +0700

    feat: src/allowscanner/scanners/__init__.py (secrets/graphql/methods/takeover modules)

--- src/allowscanner/scanners/__init__.py
+++ src/allowscanner/scanners/__init__.py
@@ -4,11 +4,15 @@ from .cookies import CookieScanner
 from .cors import CORSScanner
 from .dns import DNSScanner
 from .fuzz import FuzzScanner
+from .graphql import GraphQLScanner
 from .headers import HeaderScanner
 from .http import HttpClient
+from .methods import HttpMethodScanner
 from .ports import PortScanner
+from .secrets import SecretScanner
 from .ssl import SSLScanner
 from .subdomain import SubdomainScanner
+from .takeover import TakeoverScanner
 from .tech import TechScanner
 from .vuln import VulnerabilityScanner
 
@@ -17,11 +21,15 @@ __all__ = [
     "CookieScanner",
     "DNSScanner",
     "FuzzScanner",
+    "GraphQLScanner",
     "HeaderScanner",
     "HttpClient",
+    "HttpMethodScanner",
     "PortScanner",
     "SSLScanner",
+    "SecretScanner",
     "SubdomainScanner",
+    "TakeoverScanner",
     "TechScanner",
     "VulnerabilityScanner",
 ]
